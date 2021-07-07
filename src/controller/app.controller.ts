72daa9f60492488a56687b9c4096a38fbe1a7c05
Lucas Adams <adams.lucass@gmail.com>
2021-07-07 08:22:31 -0300

    templates viavel e nao viavel

--- src/controller/app.controller.ts
+++ src/controller/app.controller.ts
@@ -9,9 +9,22 @@ export class AppController {
   constructor(private readonly appService: AppService) {
   }
 
-  @Post('create-image')
-  async createImage(@Body() mailDto: MailDto, @Res() res: Response) {
-    const buffer: Buffer = await this.appService.createImage(mailDto);
+  @Post('create-image/viavel')
+  async createImageViavel(@Body() mailDto: MailDto, @Res() res: Response) {
+    const buffer: Buffer = await this.appService.createImageViavel(mailDto);
+    const stream = this.getReadableStream(buffer);
+
+    res.set({
+      'Content-Type': 'image/png',
+      'Content-Length': buffer.length,
+    });
+
+    stream.pipe(res);
+  }
+
+  @Post('create-image/nao-viavel')
+  async createImageNaoViavel(@Body() mailDto: MailDto, @Res() res: Response) {
+    const buffer: Buffer = await this.appService.createImageNaoViavel(mailDto);
     const stream = this.getReadableStream(buffer);
 
     res.set({
--- src/service/app.service.ts
+++ src/service/app.service.ts
@@ -5,13 +5,21 @@ import { TemplateService } from './template.service';
 @Injectable()
 export class AppService {
   private static TEMPLATE_CO2 = 'co2e.html';
+  private static TEMPLATE_CO2_NAO_VIAVEL = 'co2e-nao-viavel.html';
 
   constructor(private readonly templateService: TemplateService) {
   }
 
-  async createImage(mailDto: MailDto): Promise<Buffer> {
+  async createImageViavel(mailDto: MailDto): Promise<Buffer> {
     return await this.templateService.createImage(
       AppService.TEMPLATE_CO2,
+      mailDto,
+    );
+  }
+
+  async createImageNaoViavel(mailDto: MailDto): Promise<Buffer> {
+    return await this.templateService.createImage(
+      AppService.TEMPLATE_CO2_NAO_VIAVEL,
       mailDto,
     );
   }
